85d78b4ad3fd2afe9f45e5600b5eda7a0eabf4e4
Nathan Jaguenaud <nathan@jaguenaud.org>
2020-04-23 17:00:34 +0200

    Version ponderer ajout d'un systeme de distance.

--- src/fr/jnath/GPS/Main.java
+++ src/fr/jnath/GPS/Main.java
@@ -8,30 +8,62 @@ public class Main {
 	public Main() {
 		// Créer graph
 		Graph map = new Graph();
-		map.addPoint(new Point("A", -4, 0));
+		map.addPoint(new Point("A", 0, 0));
 		map.addPoint(new Point("B", 4, 0));
-		map.addPoint(new Point("C", 1, -4));
+		map.addPoint(new Point("C", 2, -4));
 		map.addPoint(new Point("D", -1, -4));
-		map.addSegment(new Segment(Point.search("A"), Point.search("B"), 8));
+		map.addPoint(new Point("E", 0, 0));
+		map.addPoint(new Point("F", 4, 0));
+		map.addPoint(new Point("G", 2, -4));
+		map.addPoint(new Point("H", -1, -4));
+		map.addPoint(new Point("I", 0, 0));
+		map.addPoint(new Point("J", 4, 0));
+		map.addPoint(new Point("K", 2, -4));
+		map.addPoint(new Point("L", -1, -4));
+		map.addSegment(new Segment(Point.search("A"), Point.search("B"), 3));
+		map.addSegment(new Segment(Point.search("A"), Point.search("E"), 4));
+		map.addSegment(new Segment(Point.search("A"), Point.search("D"), 2));
 		map.addSegment(new Segment(Point.search("B"), Point.search("C"), 5));
-		map.addSegment(new Segment(Point.search("C"), Point.search("D"), 2));
-		map.addSegment(new Segment(Point.search("D"), Point.search("A"), 5));
+		map.addSegment(new Segment(Point.search("B"), Point.search("F"), 4));
+		map.addSegment(new Segment(Point.search("B"), Point.search("E"), 2));
+		map.addSegment(new Segment(Point.search("B"), Point.search("D"), 1));
+		map.addSegment(new Segment(Point.search("C"), Point.search("G"), 3));
+		map.addSegment(new Segment(Point.search("C"), Point.search("F"), 2));
+		map.addSegment(new Segment(Point.search("C"), Point.search("E"), 1));
+		map.addSegment(new Segment(Point.search("D"), Point.search("E"), 2));
+		map.addSegment(new Segment(Point.search("D"), Point.search("I"), 1));
+		map.addSegment(new Segment(Point.search("D"), Point.search("H"), 2));
+		map.addSegment(new Segment(Point.search("E"), Point.search("F"), 4));
+		map.addSegment(new Segment(Point.search("E"), Point.search("J"), 1));
+		map.addSegment(new Segment(Point.search("E"), Point.search("I"), 3));
+		map.addSegment(new Segment(Point.search("F"), Point.search("G"), 3));
+		map.addSegment(new Segment(Point.search("F"), Point.search("K"), 1));
+		map.addSegment(new Segment(Point.search("F"), Point.search("J"), 1));
+		map.addSegment(new Segment(Point.search("F"), Point.search("I"), 2));
+		map.addSegment(new Segment(Point.search("G"), Point.search("L"), 3));
+		map.addSegment(new Segment(Point.search("G"), Point.search("K"), 1));
+		map.addSegment(new Segment(Point.search("G"), Point.search("J"), 2));
+		map.addSegment(new Segment(Point.search("H"), Point.search("I"), 3));
+		map.addSegment(new Segment(Point.search("I"), Point.search("J"), 1));
+		map.addSegment(new Segment(Point.search("J"), Point.search("K"), 0));
+		map.addSegment(new Segment(Point.search("J"), Point.search("L"), 2));
+		map.addSegment(new Segment(Point.search("K"), Point.search("L"), 1));
 		// InddPoint(stancier GPS
 		GPS gps = new GPS(new GraphGPS(map));
 		// Recherche chemin A -> B
 		//Path path = gps.searchPath("A","B");
-		Path chemin1 = gps.search(Point.search("A"), Point.search("D"));
+		Path chemin1 = gps.search(Point.search("A"), Point.search("L"));
 		chemin1.displayPath();
 		//display path;
 		// A -> C
-		Path chemin2 = gps.search(Point.search("A"), Point.search("C"));
-		chemin2.displayPath();
+		//Path chemin2 = gps.search(Point.search("A"), Point.search("C"));
+		/*chemin2.displayPath();
 		// B -> A
 		Path chemin3 = gps.search(Point.search("B"), Point.search("A"));
 		chemin3.displayPath();
 		// B -> D
 		Path chemin4 = gps.search(Point.search("B"), Point.search("D"));
-		chemin4.displayPath();
+		chemin4.displayPath();*/
 		
 	}
 	public static void main(String [] args) {
@@ -103,6 +135,15 @@ class Segment {
 	public Point get_pointB() {
 		return _pointB;
 	}
+	public Point get_inverse(Point point) {
+		if(point==_pointA) {
+			return _pointB;			
+		}
+		if(point==_pointB) {
+			return _pointA;			
+		}
+		else return null;
+	}
 }
 
 
@@ -170,7 +211,7 @@ class Path{
 		_pointPast.add(start);
 	}
 	public boolean addNextVerif(PointGPS point) {
-		if(point.estDejapasse) {
+		if(_pointPast.contains(point)) {
 			return false;
 		}
 		return true;
@@ -190,6 +231,24 @@ class Path{
 		}
 		System.out.println(display);
 	}
+	public Double getDist() {
+		Double distTotal = (double) 0;
+		Point lastPoint = null;
+		for (PointGPS point : _pointPast) {
+			if(point==_pointPast.get(0)) {
+				lastPoint=point.p;
+			} else {
+				for(Segment s : lastPoint.segmentWithMe()) {
+					if(s.get_inverse(lastPoint)==point.p) {
+						distTotal+=s.get_dist();
+						break;
+					}
+				}
+				lastPoint=point.p;
+			}
+		}
+		return distTotal;
+	}
 }
 class GPS {
 	private GraphGPS _map;
@@ -197,8 +256,10 @@ class GPS {
 		_map=map;
 	}
 	public Path search(Point start, Point end) {
+		int test=0;
 		List<Path> _chemins = new ArrayList<Path>();
 		List<Path> _newChemins = new ArrayList<Path>();
+		List<Path> _toEnd = new ArrayList<Path>();
 		_chemins.clear();
 		_newChemins.clear();
 		if (!(_map.pointGraph().contains(start)&& _map.pointGraph().contains(end))) {
@@ -209,6 +270,7 @@ class GPS {
 		_chemins.add(new Path(PointGPS.searchWithPoint(start.get_name())));
 		while (true) {
 			// On recherche les point adjasent du dernier point de chaque chemin on ajoute les nouveau chemin à une liste
+			
 			for(Path chemin : _chemins) {
 				for (Point pAdja :chemin.getLastPoint().p.adjasentPoint()) {
 					Path p=null;
@@ -225,20 +287,36 @@ class GPS {
 					}
 				}
 			}
+			test+=1;
 			// On vérifi si on as fini et on return le chemin sinon on reboucle
-			for (Path chemin : _newChemins){
-				if (chemin.getLastPoint().p==end){
-					for(PointGPS p : _map.pointGPSGraph()) {
-						p.estDejapasse=false;
-					}
-					return chemin;
-				}
-			}
 			_chemins.clear();
 			for(Path element : _newChemins) {
 				_chemins.add(element);
 			}
+			for (Path chemin : _newChemins){
+				//chemin.displayPath();
+				if (chemin.getLastPoint().p==end){
+					_toEnd.add(chemin);
+				 	_chemins.remove(chemin);
+				}
+			}
 			_newChemins.clear();
+			if(_chemins.size()==0) {
+				Path curent=null;
+				Path plusCout=null;
+				for(Path chemin : _toEnd) {
+					curent=chemin;
+					if (plusCout==null) {
+						plusCout=curent;
+					}
+					if (curent.getDist()<plusCout.getDist()) {
+						plusCout=curent;
+					}
+				}
+				return plusCout;
+			}
+			
+			
 		}
 	}
 }
